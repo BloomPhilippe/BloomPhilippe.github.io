4da688643fcd7ca43e9dcb35ecfa64efea0669fc
Philippe Bloom <philippe.bloom@produweb.be>
2019-02-27 17:01:40 +0100

    add react

---
+++ projets/react-form-wizard/precache-manifest.6f66fdd639ea724bf4bce8f2b17e5f63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c727a67a997c381588ae",
+    "url": "/static/css/main.6144e2cd.chunk.css"
+  },
+  {
+    "revision": "c727a67a997c381588ae",
+    "url": "/static/js/main.c727a67a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b706526af8c644678645",
+    "url": "/static/js/2.b706526a.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b628384769e1ce9a5c30b8949adfc277",
+    "url": "/index.html"
+  }
+];
--- projets/react-form-wizard/service-worker.js
+++ projets/react-form-wizard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projets/react-form-wizard/precache-manifest.ee2a3c3938ee643ea63e872a5a6ac058.js"
+  "/precache-manifest.6f66fdd639ea724bf4bce8f2b17e5f63.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/projets/react-form-wizard/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
